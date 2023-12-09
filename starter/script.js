7aa687a971b66f7e9ed79e7f4f73e81a3068999d
Gregory Oghenah <kenny@gregorys-mbp.home>
2023-12-09 21:09:52 +0000

    Implemented the Time blocks in the JS file and HTML

--- starter/script.js
+++ starter/script.js
@@ -1,16 +1,34 @@
+let txt1; let txt2; let txt3;
+
 const now = dayjs()
 
+//jquery to target the current day element 
 const currentDay = $("#currentDay")
 
+//Function to generate current day
 const updateDate = () => {
-  const currentTime = dayjs().format('DD-MM-YYYY HH:mm:ss');
+  const currentTime = now.format('MMMM D, YYYY HH:mm A');
   currentDay.text(currentTime);
 };
 updateDate();
 
 setInterval(updateDate, 1000);
 
-const workTime = ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM']
+const workTime = now.format('h:mm A');
 
-console.log(now);
 
+const workTableTime = ['9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM'];
+
+//Function to create Elements for the Timeblocks, Text Area and Save button
+function createElements() {
+
+  workTableTime.forEach((time, index) => {
+    txt1 = $("<div></div>").addClass( "col-md-1 hour" ).text(time);
+    txt2 = $("<textarea></textarea>").addClass( "col-md-10 description" );
+    txt3 = $("<button></button>").addClass("btn saveBtn col-md-1").text("Save");
+    $( "#hr9" ).append(txt1, txt2, txt3);
+
+  })
+ 
+}
+createElements();
