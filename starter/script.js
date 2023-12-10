a1cdfe4aa266efb6ccee5768b3ef3c165bd12298
Gregory Oghenah <kenny@gregorys-mbp.home>
2023-12-10 14:46:40 +0000

    refactored the create Elements for the Timeblocks, Text Area and Save button codes

--- starter/script.js
+++ starter/script.js
@@ -14,30 +14,38 @@ updateDate();
 
 setInterval(updateDate, 1000);
 
-const workTime = now.format('h:mm A');
+const workTime = now.format('h A');
 
 
 const workTableTime = ['9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM'];
+
+
 
 //Function to create Elements for the Timeblocks, Text Area and Save button
 function createElements() {
 
   workTableTime.forEach((time, index) => {
-    txt1 = $("<div></div>").addClass( "col-md-1 hour index" ).text(time);
-    txt2 = $("<textarea></textarea>").addClass( "col-md-10 description index" );
+    txt1 = $("<div></div>").addClass("col-md-1 hour").attr("id", index).text(time);
+    txt2 = $("<textarea></textarea>").addClass("col-md-10 description").attr("id", index);
     txt3 = $("<button></button>").addClass("btn saveBtn col-md-1").text("Save");
     $( "#hr9" ).append(txt1, txt2, txt3);
+  
     
-
-   
-    if (time[index] === workTime) {
-      txt2.addClass( "present" );
-    } if (time[index] < workTime) {
+    if (workTableTime[index] < workTime && workTableTime[index] != workTime ) {
       txt2.addClass( "past" );
-    } else txt2.addClass( "future" );
+    } else if (workTime < workTableTime[index]) {
+      txt2.addClass( "future" );
+    } else txt2.addClass("present");
+    
   })
+
   
 }
-
-
 createElements();
+
+
+
+
+
+
+
