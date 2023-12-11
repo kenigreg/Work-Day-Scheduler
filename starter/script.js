0ca3dbdbf6799ca5122497cd6b2c369d3963305a
Gregory Oghenah <kenny@gregorys-mbp.home>
2023-12-11 20:30:27 +0000

    Completed the Event Listener functions for User Input and saving User inout in Local Storage

--- starter/script.js
+++ starter/script.js
@@ -1,4 +1,5 @@
 let txt1; let txt2; let txt3;
+let Ind;
 
 const now = dayjs()
 
@@ -26,8 +27,8 @@ function createElements() {
 
   workTableTime.forEach((time, index) => {
     txt1 = $("<div></div>").addClass("col-md-1 hour").attr("id", index).text(time);
-    txt2 = $("<textarea></textarea>").addClass("col-md-10 description").attr("id", index);
-    txt3 = $("<button></button>").addClass("btn saveBtn col-md-1").text("Save");
+    txt2 = $("<textarea></textarea>").addClass("col-md-10 description").attr("id", "txtArea" + index);
+    txt3 = $("<button></button>").addClass("btn saveBtn col-md-1").attr("id", "btn").text("Save");
     $( "#hr9" ).append(txt1, txt2, txt3);
   
     
@@ -44,8 +45,77 @@ function createElements() {
 createElements();
 
 
+//Event Listener to get User Input
+$(document).ready(function () {
+  $("textarea").on("keydown", function (event) {
+      event.preventDefault;
+      Ind = event.target.id;
+     
+      return Ind;
+    },
+  );
 
+});
 
+//Event Listener to Save User Input in Local Stotage
+$(document).ready(function () {
+  $("button").on("click", function (event) {
+      event.preventDefault;
+
+      let val;
+
+      switch (Ind) {
+        case "txtArea0":
+          val = $("#txtArea0").val();
+          localStorage.setItem("text0", val);
+          $("#txtArea0").append(localStorage.getItem("text0"));
+          break;
+        case "txtArea1":
+          val = $("#txtArea1").val();
+          localStorage.setItem("text1", val);
+          $("#txtArea1").append(localStorage.getItem("text1"));
+          break;
+        case "txtArea2":
+          val = $("#txtArea2").val();
+          localStorage.setItem("text2", val);
+          $("#txtArea2").append(localStorage.getItem("text2"));
+          break;
+        case "txtArea3":
+          val = $("#txtArea3").val();
+          localStorage.setItem("text3", val);
+          $("#txtArea3").append(localStorage.getItem("text3"));
+          break;
+        case "txtArea4":
+          val = $("#txtArea4").val();
+          localStorage.setItem("text4", val);
+          $("#txtArea4").append(localStorage.getItem("text4"));
+          break;
+        case "txtArea5":
+          val = $("#txtArea5").val();
+          localStorage.setItem("text5", val);
+          $("#txtArea5").append(localStorage.getItem("text5"));
+          break;
+        case "txtArea6":
+          val = $("#txtArea6").val();
+          localStorage.setItem("text6", val);
+          $("#txtArea6").append(localStorage.getItem("text6"));
+          break;
+        case "txtArea7":
+          val = $("#txtArea7").val();
+          localStorage.setItem("text7", val);
+          $("#txtArea7").append(localStorage.getItem("text7"));
+          break;
+        case "txtArea8":
+          val = $("#txtArea8").val();
+          localStorage.setItem("text8", val);
+          $("#txtArea8").append(localStorage.getItem("text8"));
+      }
+      
+  }
+  
+      );
+
+});
 
 
 
